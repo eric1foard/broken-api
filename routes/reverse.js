e9ab4d488c99c59d94bdbb1551d14128e793dbd1
Eric Foard <foard265@gmail.com>
2022-10-17 15:08:01 -0700

    fix bug in /reverse endpoint in which parseInt is called on string values

--- routes/reverse.js
+++ routes/reverse.js
@@ -1,14 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 function isNumber(str) {
-  return typeof parseInt(str) === 'NaN';
+  return typeof parseInt(str) === "NaN";
 }
 
-router.get('/', function(req, res, next) {
-  var reversed = req.query.str.split('').reverse().join('');
+router.get("/", function (req, res, next) {
+  var reversed = [...req.query.str].reverse().join("");
   res.json({
-  	reversed: (isNumber(req.query.str)) ? reversed : parseInt(reversed)
+    reversed: isNumber(req.query.str) ? parseInt(reversed) : reversed,
   });
 });
 
